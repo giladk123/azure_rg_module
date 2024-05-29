a12eeced316e7a87279a1271ab4bd516b7b1bc9e
Gilad <giladk@gmail.com>
2024-05-29 08:59:44 +0300

    changed subscription naming convention

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   json_data         = jsondecode(file("./cac/resource_groups.json"))
   resource_groups   = local.json_data.resource_groups
   subscription_id   = lookup({ for s in data.azurerm_subscriptions.available.subscriptions : s.display_name => s.subscription_id... }, local.subscription_name, [""])[0]
-  subscription_name = local.json_data.subscription_info.subscription_cmdb
+  subscription_name = "${local.region}-${local.tenant_name}${local.environment}-azus-${local.json_data.subscription_info.subscription_cmdb}-${local.json_data.subscription_info.subscription_freetext}-sb"
   region            = local.json_data.naming_convention.region
   tenant_name       = local.json_data.naming_convention.tenant_name
   environment       = local.json_data.naming_convention.environment
